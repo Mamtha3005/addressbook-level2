3c8afe1b3468c74de9e4438ca4fb6d200529b507
DESKTOP-Q2E8UPT\Mamtha <mamtha3005@gmail.com>
2016-09-06 17:17:31 -0700

    Revert "T4A4"
    This reverts commit 106478be81a81ed355079710c99be4c12f343af9.

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -1,100 +1,87 @@
 package seedu.addressbook.data.person;
 
 import seedu.addressbook.data.tag.UniqueTagList;
-import java.util.LinkedList;
+
 import java.util.Objects;
 
 /**
- * Represents a Person in the address book. Guarantees: details are present and
- * not null, field values are validated.
+ * Represents a Person in the address book.
+ * Guarantees: details are present and not null, field values are validated.
  */
 public class Person implements ReadOnlyPerson {
 
-	private Name name;
-	private Phone phone;
-	private Email email;
-	private Address address;
+    private Name name;
+    private Phone phone;
+    private Email email;
+    private Address address;
 
-	private final UniqueTagList tags;
+    private final UniqueTagList tags;
+    /**
+     * Assumption: Every field must be present and not null.
+     */
+    public Person(Name name, Phone phone, Email email, Address address, UniqueTagList tags) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+    }
 
-	/**
-	 * Assumption: Every field must be present and not null.
-	 */
-	public Person(Name name, Phone phone, Email email, Address address, UniqueTagList tags) {
-		this.name = name;
-		this.phone = phone;
-		this.email = email;
-		this.address = address;
-		this.tags = new UniqueTagList(tags); // protect internal tags from
-												// changes in the arg list
-	}
+    /**
+     * Copy constructor.
+     */
+    public Person(ReadOnlyPerson source) {
+        this(source.getName(), source.getPhone(), source.getEmail(), source.getAddress(), source.getTags());
+    }
 
-	/**
-	 * Copy constructor.
-	 */
-	public Person(ReadOnlyPerson source) {
-		this(source.getName(), source.getPhone(), source.getEmail(), source.getAddress(), source.getTags());
-	}
+    @Override
+    public Name getName() {
+        return name;
+    }
 
-	@Override
-	public Name getName() {
-		return name;
-	}
+    @Override
+    public Phone getPhone() {
+        return phone;
+    }
 
-	@Override
-	public Phone getPhone() {
-		return phone;
-	}
+    @Override
+    public Email getEmail() {
+        return email;
+    }
 
-	@Override
-	public Email getEmail() {
-		return email;
-	}
+    @Override
+    public Address getAddress() {
+        return address;
+    }
 
-	@Override
-	public Address getAddress() {
-		return address;
-	}
+    @Override
+    public UniqueTagList getTags() {
+        return new UniqueTagList(tags);
+    }
 
-	@Override
-	public UniqueTagList getTags() {
-		return new UniqueTagList(tags);
-	}
+    /**
+     * Replaces this person's tags with the tags in the argument tag list.
+     */
+    public void setTags(UniqueTagList replacement) {
+        tags.setTags(replacement);
+    }
 
-	/**
-	 * Replaces this person's tags with the tags in the argument tag list.
-	 */
-	public void setTags(UniqueTagList replacement) {
-		tags.setTags(replacement);
-	}
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ReadOnlyPerson // instanceof handles nulls
+                && this.isSameStateAs((ReadOnlyPerson) other));
+    }
 
-	@Override
-	public boolean equals(Object other) {
-		return other == this // short circuit if same object
-				|| (other instanceof ReadOnlyPerson // instanceof handles nulls
-						&& this.isSameStateAs((ReadOnlyPerson) other));
-	}
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, phone, email, address, tags);
+    }
 
-	@Override
-	public int hashCode() {
-		// use this method for custom fields hashing instead of implementing
-		// your own
-		return Objects.hash(name, phone, email, address, tags);
-	}
+    @Override
+    public String toString() {
+        return getAsTextShowAll();
+    }
 
-	@Override
-	public String toString() {
-		return getAsTextShowAll();
-	}
-
-	/**
-	 * Returns a concatenated version of the printable strings of each object.
-	 */
-	public String getPrintableString(Printable... printables) {
-		String result = "";
-		for (Printable p : printables) {
-			result += p.getPrintableString() + ", ";
-		}
-		return (String) result.subSequence(0, result.length() - 2);
-	}
 }
